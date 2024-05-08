9c717eba10eb6605e26fe2fc53c7e1e50a7ffc14
Rosshoven <Michaeledwardross@gmail.com>
2024-05-08 16:49:21 -0500

    using passport for oauth and cookies

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Calling the root file or "startup" file index.js 
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+
 
 // need to put the model class above passport, because passport will make use of the user Model. Order of operations.
 // order of require statements can give error messages.
@@ -9,21 +12,31 @@ require ('./models/User')
 require('./services/passport');
 
 
-
-// get connection to mongodDB - pass the address of the mongo instance
+// get connection to mongodDB using mongoURI- pass the address of the mongo instance
 mongoose.connect(keys.mongoURI);
 
 // get the routes
-// const authRoutes = require('./routes/authRoutes');
 const app = express();
 
+// make a statement that tells express it needs to use cookies inside our application
+app.use(
+    cookieSession({
+        // how long this cookie can exist inside the browser before expired - 30 days in milliseconds
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// const authRoutes = require('./routes/authRoutes');
+// authRoutes(app);
 require('./routes/authRoutes')(app);
 
-// authRoutes(app);
 
 
 app.get('/', (req, res) => {
-    res.send({boy: 'bye bye bye'})
+    res.send({boys: 'dont cry'})
 })
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 // passport gives express the idea of how to handle authentication
 const passport = require('passport');
-// Strategy instructs passport on how to autghenticate our users with oauth
+// Strategy instructs passport on how to authenticate our users with oauth
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
@@ -10,6 +10,22 @@ const mongoose = require('mongoose');
 // get access to user model class
 const User = mongoose.model('users')
 
+// user is whatever we pulled from our DB
+// serializeUser is built in method for passport.js
+passport.serializeUser((user, done) => {
+    // done is a callback we have to call, 1st arg err obj - null is for no error, user.id is identofying piece of info
+    done(null, user.id);
+    // console.log(user.id);
+})
+
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then((user) => {
+        done(null, user);
+    })
+});
+
 
 // New instance of the google passport strategy. setting it up. takes in the keys
 passport.use(new GoogleStrategy({
@@ -17,10 +33,24 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, 
+// second argument is a callback function that gets invoked when the user is authenticated
     (accessToken, refreshToken, profile, done) => {
         // get access to mongoose model and create new instance
-        new User({ googleId: profile.id }).save();
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+            if (existingUser) {
+            // we already have that user, done()'s 1st argument is error, here is null cause it worked out, 2nd arg is the user we found
+            done(null, existingUser);
+        } else {
+            // we don;t have auser record with thi iD, make new record
+            // this creates a mongoose model instance, reps a single record in the collection
+            new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user))
+        }
+        });
 
+        
 
         console.log('access token', accessToken);
         console.log('refresh toekn', refreshToken);
